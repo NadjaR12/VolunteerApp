ff16f1f413c72fcac8d5d1d9ad5d8f76309b5278
Lorelai <nadja.rathenow@rub.de>
2022-04-03 11:42:19 +0200

    netli redirectrs

--- client/src/pagesVisitor/Home.js
+++ client/src/pagesVisitor/Home.js
@@ -11,7 +11,6 @@ export default function Home() {
 
   return (
   <>
-  <h1>test</h1>
     <div className="home-container fixed-background">
     <div className="home-overlay fixed-background">
       <div className="logo-container">
